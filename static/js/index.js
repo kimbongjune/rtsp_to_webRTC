1c559f03cec786c6cc15839b34c17694cb2fe964
김봉준 <nocdu112@naver.com>
2023-10-13 15:29:02 +0900

    중복연결 방지 로직 추가

--- static/js/index.js
+++ static/js/index.js
@@ -1,91 +1,125 @@
-/*
- * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
+/**
+ * 
+ * 영상 변환 클라이언트 예제 파일
+ * 무선호출명을 이용해 서버에서 해당하는 rtsp ip를 조회하고 ip와 port를 이용해 tcp health 체크를 진행한다.
+ * 정상이 반환되면 서버와 통신해 webRTC 영상을 재생한다.
+ * 
+ * @author kbj.
+ * @since 2023-10-13
+ * @version 1.0.0
+ * 
+ * <pre>
+ * << 개정이력(Modefication Information) >>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 수정일       		수정자      수정내용 
+ * ================================= 
+ * 2023-10-13   kbj.    최초생성 
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * </pre>
  *
  */
 
+//소켓 서버와 연결 초기화
 let socket = io.connect(location.host);
+//비디오 엘리먼트를 위한 변수
 let video;
+//여러 WebRTC 피어를 관리할 객체
 let webRtcPeer = {}
-let streamingName = '';
 
+//소켓 메시지 전송 공통함수
 const sendMessage = (id, message) =>{
     socket.emit(id, message);
 }
 
+//html 로딩 완료 이벤트
 window.onload = () => {
-    //console = new Console();
     video = document.getElementById('video');
 
-    document.getElementById('viewer').addEventListener('click', function() { rtcConnect(); });
+    document.getElementById('viewer').addEventListener('click', function() { 
+        rtcConnect();   //RTC 연결 시작
+    });
+
     document.getElementById('terminate').addEventListener('click', function() { 
         const uuid = this.getAttribute('data-uuid');
         if(uuid) {
-            stop(uuid);
+            stop(uuid); //연결 종료
         }
     });
 }
 
+
+//페이지 이동 이벤트(새로고침, 닫기, 페이지 이동)
 window.onbeforeunload = () => {
-    Object.keys(webRtcPeer).forEach(stop);
+    Object.keys(webRtcPeer).forEach(stop);  //모든 피어 커넥션 종료
 }
 
+//webRTC sdpAnswer 응답 함수
 const viewerResponse = (message) => {
-    if (message.response != 'accepted') {
+    if (message.response != 'accepted') {   //에러시 연결 종료
         const errorMsg = message.message ? message.message : 'Unknow error';
+        console.log(message)
         console.warn('Call not accepted for the following reason: ' + errorMsg);
         dispose();
-    } else {
+    } else {    //정상 연결시 sdpAnswer를 webRtcPeer에 추가 및 재생
         webRtcPeer[message.streamUUID].processAnswer(message.sdpAnswer);
     }
 }
 
+//RTC 연결 요청 함수
 const rtcConnect = () =>{
-	streamingName = document.getElementById("streaming_name").value;
-
-    if (!socket.connected) {
-        socket.connect();
+    //중복 연결 방지
+    if (Object.values(webRtcPeer).some(peer => peer.streamingName === streamingName)) {
+        return;
     }
+
+	const streamingName = document.getElementById("streaming_name").value;  //무선호출명을 파라미터로 전송
+
+    //무선 호출명 빈 문자열 검증
     if (!streamingName) {
         alert('Please enter a valid RTSP URL');
         return;
     }
+
+    //소켓 연결 확인 및 재연결
+    if (!socket.connected) {
+        socket.connect();
+    }
+    
     const message = {streamingName: streamingName}
     sendMessage("rtcConnect", message)
 }
 
+//webRTC 연결 설정 함수
 const viewer = (streamUUID, rtspIp, streamingName) => {
+    //중복 연결 방지
     if (!webRtcPeer[streamUUID]) {
         showSpinner(video);
 
+        //소켓 연결 확인 및 재연결
         if (!socket.connected) {
             socket.connect();
         }
 
+        //영상을 응답받을 비디오 엘리먼트와 ICE Candidate 처리 이벤트 리스너 등록
         const options = {
             remoteVideo: video,
             onicecandidate: (candidate) => onIceCandidate(candidate, streamUUID)
         }
 
+        //서버에서 발급받은 streamUUID를 이용해 WebRTC 피어생성(영상 수신 전용)
         webRtcPeer[streamUUID] = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
             if (error) return onError(error);
 
+            //offer 생성
             this.generateOffer((error, offerSdp) => onOfferViewer(error, offerSdp, rtspIp, streamingName, streamUUID));
         });
+
+        //WebRTC 피어에 무선호출명 객체를 추가해 중복 호출 방지
+        webRtcPeer[streamUUID].streamingName = streamingName;
     }
 }
 
+//offer를 서버에 전송
 const onOfferViewer = (error, offerSdp, rtspIp, streamingName, streamUUID) => {
     if (error) return onError(error);
 
@@ -100,6 +134,7 @@ const onOfferViewer = (error, offerSdp, rtspIp, streamingName, streamUUID) => {
     sendMessage("viewer", message)
 }
 
+//ICE Candidate 처리 이벤트 리스너
 const onIceCandidate = (candidate, streamUUID) => {
     const message = {
         candidate: candidate,
@@ -108,6 +143,7 @@ const onIceCandidate = (candidate, streamUUID) => {
     sendMessage("onIceCandidate", message)
 }
 
+//연결 종료 함수
 const stop = (streamUUID) => {
     console.log("Stopping stream with UUID:", streamUUID);
     if (webRtcPeer[streamUUID]) {
@@ -119,6 +155,7 @@ const stop = (streamUUID) => {
     }
 }
 
+//WebRTC 피어 및 소켓 정리 함수
 const dispose = (streamUUID) => {
     if (webRtcPeer[streamUUID]) {
         webRtcPeer[streamUUID].dispose();
@@ -128,6 +165,7 @@ const dispose = (streamUUID) => {
     hideSpinner(video);
 }
 
+//비디오 엘리먼트의 백그라운드 변경 및 스피너를 보여주는 함수
 const showSpinner = (...arguments) => {
     for (let i = 0; i < arguments.length; i++) {
         arguments[i].poster = './img/transparent-1px.png';
@@ -135,6 +173,7 @@ const showSpinner = (...arguments) => {
     }
 }
 
+//비디오 엘리먼트의 백그라운드 변경 및 스피너를 제거하는 함수
 const hideSpinner = (...arguments) => {
     for (let i = 0; i < arguments.length; i++) {
         arguments[i].src = '';
@@ -142,12 +181,14 @@ const hideSpinner = (...arguments) => {
     }
 }
 
+//webRTC 통신간 발생한 에러 처리 함수
 const onError = (error) => {
     console.error(error)
     Object.keys(webRtcPeer).forEach(stop);
     alert("error: ", error)
 }
 
+//websocket 연결 성공 리스너
 socket.on('connect', function() {
     console.log('Connected to the server.');
     if(!socket){
@@ -155,25 +196,37 @@ socket.on('connect', function() {
     }
 });
 
-// 'connect_error' 이벤트는 연결에 오류가 발생했을 때 발생합니다.
+//websocket 연결 오류 리스너
 socket.on('connect_error', function(error) {
     console.log(error);
     Object.keys(webRtcPeer).forEach(stop);
 });
 
+//websocket 연결 종료 리스너
 socket.on('disconnect', function() {
     Object.keys(webRtcPeer).forEach(stop);
 });
 
+//websocket 에러 리스너
 socket.on('error', (error) =>{
 	console.error(error)
 });
 
+//websocket viewerResponse 응답 처리 리스너, sdpAnswer를 가지고있음
 socket.on('viewerResponse', viewerResponse);
-socket.on('stopCommunication', dispose);
+
+//websocket error 응답 처리 리스너, 모든 피어 연결을 종료함
+socket.on('error', (data) =>{
+    console.log(data)
+    Object.keys(webRtcPeer).forEach(stop)
+});
+
+//websocket ICE Candidate 응답 처리 리스너, ICE Candidate 후보에 추가함
 socket.on('iceCandidate', (data) => {
     webRtcPeer[data.streamUUID].addIceCandidate(data.candidate);
 });
+
+//websocket rtcConnect 응답 처리 리스너, 전송한 무선호출명에 대한 응답 파라미터를 가지고있음
 socket.on('rtcConnectResponse', (data) => {
     if(data.response === "success"){
 		console.log(data)
